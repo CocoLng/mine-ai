c114c742a247302eb5e39f297d321193abe53e37
CocoLng <corentin.lange@hotmail.fr>
2023-01-11 23:54:23 +0100

    GitIgnore Done

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ print("Hello World, I'm implementing Actions")
 print("Writing this for getting a linter check implementation")
 
 # Present the project
-# This project is about implementing an AI whith Q-Learning tables in Minecraft
+# This project is about implementing an AI with Q-Learning tables in Minecraft
 # The AI will be able to learn how to play Minecraft by itself
 # The goal is to make it play the game and get the best score possible
 # Further idea are show on the project view : https://github.com/users/CocoLng/projects/1
