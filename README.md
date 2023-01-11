64fc9f4c20538eecdcc2a6052df3a45fbf60b82b
CocoLng <corentin.lange@hotmail.fr>
2023-01-12 00:32:07 +0100

    HowTo

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Mine AI
 
 
-This project is about implementing an **AI** whith Q-Learning tables in **Minecraft**
+This project is about implementing an **AI** with Q-Learning tables in **Minecraft**.
 
 <p align="center">
 <img src="https://github.com/CocoLng/mine-ai/actions/workflows/linter.yml/badge.svg" alt="Lint Code" />
@@ -14,6 +14,31 @@ The project is divided in 3 milestones :
 - **1** How to get the data from the game
 - **2** How to implement the AI with basic rules set
 - **3** How to implement the AI with Q-Learning tables
+
+### 1. How to get the data from the game
+* **1.1** How to get the map
+* **1.2** How to get the player's position
+* **1.3** How to get the player's inventory
+* **1.4** How to get the player's health
+* **1.5** How to get the player's hunger
+* **1.6** How to get the player's experience
+
+### 2. How to implement the AI with basic rules set
+* **2.1** How to make the player move
+* **2.2** How to make the player break blocks
+* **2.3** How to make the player place blocks
+* **2.4** How to make the player eat
+* **2.5** How to make the player attack
+
+### 3. How to implement the AI with Q-Learning tables
+* **3.1** How to make the AI learn
+* **3.2** How to make the AI use the Q-Learning tables
+* **3.3** How to make the AI use the Q-Learning tables with the player's inventory
+* **3.4** How to make the AI use the Q-Learning tables with the player's health
+* **3.5** How to make the AI use the Q-Learning tables with the player's hunger
+* **3.6** How to make the AI use the Q-Learning tables with the player's experience
+* **3.7** How to make the AI use the Q-Learning tables with the player's position
+
 
 ## Current Features
 
--- main.py
+++ main.py
@@ -1,13 +1,39 @@
-print("Hello World, I'm implementing Actions")
-print("Writing this for getting a linter check implementation")
-
-# Present the project
-# This project is about implementing an AI with Q-Learning tables in Minecraft
-# The AI will be able to learn how to play Minecraft by itself
-# The goal is to make it play the game and get the best score possible
-# Further idea are show on the project view : https://github.com/users/CocoLng/projects/1
-
 # The project is divided in 3 parts :
 # 1. How to get the data from the game
 # 2. How to implement the AI with basic rules set
 # 3. How to implement the AI with Q-Learning tables
+
+# 1. How to get the data from the game
+# The first step is to get the data from the game
+# This is the most difficult part of the project
+# The data can be get from the game in 2 ways :
+# 1.1. By using a library like OpenAI Gym
+# 1.2. By using the Minecraft API
+
+# 1.1. By using a library like OpenAI Gym
+# The first way to get data from the game is by using a library like OpenAI Gym
+# The problem is that OpenAI Gym is not made for Minecraft
+# Moreover, I don't know if it is possible to use OpenAI Gym for Minecraft
+# The first step is to find a way to use OpenAI Gym for Minecraft
+# If it is not possible to use OpenAI Gym for Minecraft, I will use the Minecraft API
+
+# 1.2. By using the Minecraft API
+# The second way to get data from the game is by using the Minecraft API
+# The Minecraft API is made for Minecraft and is very easy to use
+# The problem is that the Minecraft API is not made for AI
+# The first step is to find a way to use the Minecraft API for AI
+# If it is not possible to use the Minecraft API for AI, I will use OpenAI Gym
+
+# 2. How to implement the AI with basic rules set The second step is to implement the AI with basic rules set The
+# basic rules set will be very easy to implement The basic rules set will be made for the first version of the AI The
+# first version of the AI will be very simple The first version of the AI will be able to play Minecraft but it will
+# not be good The first version of the AI will be able to play Minecraft but it will not be able to get a good score
+# The first version of the AI will be able to play Minecraft but it will not be able to learn The first version of
+# the AI will be able to play Minecraft but it will not be able to learn how to play Minecraft The first version of
+# the AI will be able to play Minecraft but it will not be able to learn how to get a good score The first version of
+# the AI will be able to play Minecraft but it will not be able to learn how to play Minecraft and get a good score
+
+# 3. How to implement the AI with Q-Learning tables
+# The third step is to implement the AI with Q-Learning tables
+# The Q-Learning tables will be very difficult to implement
+# The Q-Learning tables will be made for the second version of the AI
