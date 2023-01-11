1cfc07b3f0c88ac078615131deaca5d24caacd48
CocoLng <15895046+CocoLng@users.noreply.github.com>
2023-01-11 20:28:56 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 print("Hello World, I'm implementing Actions")
-print("Writingthis for getting a linter check implementation")
+print("Writing this for getting a linter check implementation")
+
+# Present the project
+# This project is about implementing an AI whith Q-Learning tables in Minecraft
+# The AI will be able to learn how to play Minecraft by itself
+# The goal is to make it play the game and get the best score possible
+# Further idea are show on the project view : https://github.com/users/CocoLng/projects/1
+
+# The project is divided in 3 parts :
+# 1. How to get the data from the game
+# 2. How to implement the AI with basic rules set
+# 3. How to implement the AI with Q-Learning tables
